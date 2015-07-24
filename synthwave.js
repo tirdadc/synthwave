d9892294c92de5ff3427851753c0ad6928f68f20
tirdadc <tirdadck@gmail.com>
2015-07-24 17:09:24 -0400

    Added some new words + suffixes

--- synthwave.js
+++ synthwave.js
@@ -14,6 +14,7 @@ var words = [
   'Vindicator',
   'Protector',
   'Vice',
+  'Video',
   'Hotline',
   'Skyline',
   'Streets',
@@ -78,6 +79,9 @@ var suffixes = [
   'drive',
   'rider',
   'slasher',
+  'driver',
+  'crawler',
+  'hunter',
   'net'
 ];
 
